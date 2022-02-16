7941f32ccc5bcd3f8aa0ff3d85ed04d6299263c9
edtallison <edtallison@gmail.com>
2022-02-16 22:04:47 +1300

    Add fishing info

--- commands/bing.js
+++ commands/bing.js
@@ -6,9 +6,9 @@ module.exports = {
         const ideal = 1 + Math.floor(Math.random() * 100); // 1 to 100 (inclusive)
 
         if (ideal < 50) {
-            await interaction.reply('Today **is not** an ideal fishing day.');
+            await interaction.reply('Today **is not** an ideal fishing day. <:193yywvl7rj41:931473656318668810>');
         } else {
-            await interaction.reply('Today **is** an ideal fishing day!');
+            await interaction.reply('Today **is** an ideal fishing day! <:dreamCD:810112018606456857>');
         }
     },
 };
